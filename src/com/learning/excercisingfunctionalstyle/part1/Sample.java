0ce2d9443bcb8371433b4de2fc21703346d55c99
vinayrshanbhag <vinay.shanbhag@live.com>
2017-08-28 11:11:40 +0530

    Excercising functional style

---
+++ src/com/learning/excercisingfunctionalstyle/part1/Sample.java
@@ -0,0 +1,31 @@
+package com.learning.excercisingfunctionalstyle.part1;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class Sample {
+
+	 public static void main(String[] args) {
+		 
+		 ExecutorService executorService = Executors.newFixedThreadPool(10);
+		 
+		IntStream.range(1, 10)
+		         .forEach(i->executorService.submit(()->
+		                  System.out.println("Running task "+ i)));
+		 
+		/* for(int i=0;i<10;i++){
+			 //final int index =i;
+			 //effectivly final
+			 int index =i;
+			 executorService.submit(()->System.out.println("Running task "+ index));
+		 }*/
+		 
+		 System.out.println("Task started...");
+	     executorService.shutdown();
+		 
+		  
+	}
+	
+	
+}
---
+++ src/com/learning/excercisingfunctionalstyle/part3/FunctionalSample.java
@@ -0,0 +1,10 @@
+package com.learning.excercisingfunctionalstyle.part3;
+
+public class FunctionalSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

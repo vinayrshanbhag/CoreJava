bcf789629a4d21f00cb6d8e64f12c3dddb4fb3a9
vinayrshanbhag <vinay.shanbhag@live.com>
2017-08-28 11:22:38 +0530

    Functional style optional

--- src/com/learning/excercisingfunctionalstyle/part3/FunctionalSample.java
+++ src/com/learning/excercisingfunctionalstyle/part3/FunctionalSample.java
@@ -1,9 +1,38 @@
 package com.learning.excercisingfunctionalstyle.part3;
 
-public class FunctionalSample {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+class TimeSlot1{
+	 
+	 static Random random = new Random(System.nanoTime());
+	 
+	 public String schedule(){
+		 return "booking details..." + hashCode();
+	 }
+	 
+	 public boolean isAvailable(){
+		 return random.nextBoolean();
+	 }
+	 
+	
+}
+
+public class FunctionalSample {
+	
+	
+	public static void main(String[] args) {}{
+		List<TimeSlot> timeSlots = ImpertativeSample.createTimeSlots();
+		
+		System.out.println(timeSlots.stream()
+				                    .filter(TimeSlot::isAvailable)
+				                    .findFirst()
+				                    .map(TimeSlot::schedule)
+				                    .orElse("not available"));
+		                            
+		
 
 	}
 

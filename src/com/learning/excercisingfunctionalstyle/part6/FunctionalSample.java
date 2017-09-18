162f484fa0e74e901b0ab67948729b4282d02576
vinayrshanbhag <vinay.shanbhag@live.com>
2017-09-18 08:56:39 +0530

    Usual Commit

---
+++ src/com/learning/excercisingfunctionalstyle/part6/FunctionalSample.java
@@ -0,0 +1,22 @@
+package com.learning.excercisingfunctionalstyle.part6;
+
+import java.util.stream.IntStream;
+
+public class FunctionalSample {
+	
+	public static boolean isPrime(int number) {
+		
+		return number>1 && IntStream.range(1, number)
+				                    .noneMatch(i->number%i==0);
+	} 
+
+
+	public static void main(String[] args) {
+		
+		IntStream.range(1, 8)
+		         .forEach(i-> System.out.printf("isPrime(%d)? (%b)\n",i,isPrime(i)));
+		
+		
+	}
+
+}
